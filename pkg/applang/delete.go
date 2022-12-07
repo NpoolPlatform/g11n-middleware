484d3c8cb85fdc7cd4d48015852b538a15b92c30
Zhao KK <kikakkz@hotmail.com>
2022-12-07 10:40:48 +0000

    Support delete applang

--- pkg/applang/delete.go
+++ pkg/applang/delete.go
@@ -4,8 +4,20 @@ import (
 	"context"
 
 	npool "github.com/NpoolPlatform/message/npool/g11n/mw/v1/applang"
+
+	applangmgrcli "github.com/NpoolPlatform/g11n-middleware/pkg/client/applang"
 )
 
 func DeleteLang(ctx context.Context, id string) (*npool.Lang, error) {
-	return nil, nil
+	info, err := GetLang(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = applangmgrcli.DeleteLang(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	return info, nil
 }
