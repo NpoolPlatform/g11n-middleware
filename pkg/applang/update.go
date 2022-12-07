57658cef1c875f087e2b9375f62d60ad9c8cfac1
Zhao KK <kikakkz@hotmail.com>
2022-12-07 10:23:46 +0000

    Support update applang

--- pkg/applang/update.go
+++ pkg/applang/update.go
@@ -5,8 +5,14 @@ import (
 
 	applangmgrpb "github.com/NpoolPlatform/message/npool/g11n/mgr/v1/applang"
 	npool "github.com/NpoolPlatform/message/npool/g11n/mw/v1/applang"
+
+	applangmgrcli "github.com/NpoolPlatform/g11n-middleware/pkg/client/applang"
 )
 
 func UpdateLang(ctx context.Context, in *applangmgrpb.LangReq) (*npool.Lang, error) {
-	return nil, nil
+	info, err := applangmgrcli.UpdateLang(ctx, in)
+	if err != nil {
+		return nil, err
+	}
+	return GetLang(ctx, info.ID)
 }
