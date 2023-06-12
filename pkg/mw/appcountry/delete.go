4dc0ff67009915ffe0870ea334b92aeb3b918d8d
jakys <jakysing@163.com>
2023-06-12 13:43:11 +0000

    Fix appid and id param check

--- pkg/mw/appcountry/delete.go
+++ pkg/mw/appcountry/delete.go
@@ -9,10 +9,17 @@ import (
 	"github.com/NpoolPlatform/g11n-middleware/pkg/db/ent"
 
 	appcountrycrud "github.com/NpoolPlatform/g11n-middleware/pkg/crud/appcountry"
+	cruder "github.com/NpoolPlatform/libent-cruder/pkg/cruder"
 	npool "github.com/NpoolPlatform/message/npool/g11n/mw/v1/appcountry"
 )
 
 func (h *Handler) DeleteCountry(ctx context.Context) (*npool.Country, error) {
+	if h.ID == nil {
+		return nil, fmt.Errorf("invalid id")
+	}
+	if h.AppID == nil {
+		return nil, fmt.Errorf("invalid appid")
+	}
 	info, err := h.GetCountry(ctx)
 	if err != nil {
 		return nil, err
@@ -22,6 +29,17 @@ func (h *Handler) DeleteCountry(ctx context.Context) (*npool.Country, error) {
 	}
 
 	err = db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
+		h.Conds = &appcountrycrud.Conds{
+			AppID: &cruder.Cond{Op: cruder.EQ, Val: *h.AppID},
+			ID:    &cruder.Cond{Op: cruder.EQ, Val: *h.ID},
+		}
+		exist, err := h.ExistAppCountryConds(ctx)
+		if err != nil {
+			return err
+		}
+		if !exist {
+			return fmt.Errorf("appcountry not exist")
+		}
 		now := uint32(time.Now().Unix())
 		if _, err := appcountrycrud.UpdateSet(
 			cli.AppCountry.UpdateOneID(*h.ID),
--- pkg/mw/applang/delete.go
+++ pkg/mw/applang/delete.go
@@ -9,10 +9,17 @@ import (
 	"github.com/NpoolPlatform/g11n-middleware/pkg/db/ent"
 
 	applangcrud "github.com/NpoolPlatform/g11n-middleware/pkg/crud/applang"
+	cruder "github.com/NpoolPlatform/libent-cruder/pkg/cruder"
 	npool "github.com/NpoolPlatform/message/npool/g11n/mw/v1/applang"
 )
 
 func (h *Handler) DeleteLang(ctx context.Context) (*npool.Lang, error) {
+	if h.ID == nil {
+		return nil, fmt.Errorf("invalid id")
+	}
+	if h.AppID == nil {
+		return nil, fmt.Errorf("invalid appid")
+	}
 	info, err := h.GetLang(ctx)
 	if err != nil {
 		return nil, err
@@ -22,6 +29,17 @@ func (h *Handler) DeleteLang(ctx context.Context) (*npool.Lang, error) {
 	}
 
 	err = db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
+		h.Conds = &applangcrud.Conds{
+			AppID: &cruder.Cond{Op: cruder.EQ, Val: *h.AppID},
+			ID:    &cruder.Cond{Op: cruder.EQ, Val: *h.ID},
+		}
+		exist, err := h.ExistAppLangConds(ctx)
+		if err != nil {
+			return err
+		}
+		if !exist {
+			return fmt.Errorf("applang not exist")
+		}
 		now := uint32(time.Now().Unix())
 		if _, err := applangcrud.UpdateSet(
 			cli.AppLang.UpdateOneID(*h.ID),
